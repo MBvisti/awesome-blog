a93f619c0798a2e5b3443b823b14a286ece88c98
MBvisti <mbv@mortenvistisen.com>
2022-08-28 13:49:44 +0200

    wip

--- src/handlers/home_handler.rs
+++ src/handlers/home_handler.rs
@@ -1,5 +1,7 @@
 use actix_web::{get, web, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
+use std::{fs, io::Error}; 
+use ignore::WalkBuilder; 
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Frontmatter {
@@ -11,6 +13,41 @@ pub struct Frontmatter {
     author: String,
     estimated_reading_time: u32,
     order: u32,
+}
+
+fn find_all_frontmatters() -> Result<Vec<Frontmatter>, std::io::Error> {
+    let mut t = ignore::types::TypesBuilder::new();
+    t.add_defaults();
+    let toml = match t.select("toml").build() {
+	  Ok(t)=> t,
+	  Err(e)=> {
+		println!("{:}", e);
+		return Err(Error::new(std::io::ErrorKind::Other,
+		"could not build toml file type matcher"))
+	  }
+    };
+    
+    let file_walker = WalkBuilder::new("./posts").types(toml).build();
+
+    let mut frontmatters = Vec::new();
+    for frontmatter in file_walker {
+        match frontmatter {
+	      Ok(fm) => {
+		    if fm.path().is_file() {
+		        let fm_content = fs::read_to_string(fm.path())?;
+			  let frontmatter: Frontmatter = toml::from_str(&fm_content)?;
+			  
+			  frontmatters.push(frontmatter);
+		    }
+		}
+		Err(e) => {
+		    println!("{:}", e); // we're just going to print the error for now
+		    return Err(Error::new(std::io::ErrorKind::NotFound, "could not locate frontmatter"))
+		}
+	  }
+    }
+    
+    Ok(frontmatters)
 }
 
 #[get("/")]
